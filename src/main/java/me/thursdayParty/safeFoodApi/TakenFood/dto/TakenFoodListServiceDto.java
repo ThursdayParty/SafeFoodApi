3bc043c27c6bf913ce4e397fff1ff8a4fd174df3
bactoria <bactoria@gmail.com>
2019-11-28 09:29:09 +0900

    BUG :: 섭취 식품 리스트 요청 시 제조사가 없는 버그 수정

--- src/main/java/me/thursdayParty/safeFoodApi/TakenFood/dto/TakenFoodListServiceDto.java
+++ src/main/java/me/thursdayParty/safeFoodApi/TakenFood/dto/TakenFoodListServiceDto.java
@@ -20,6 +20,7 @@ public class TakenFoodListServiceDto {
     private Long foodId;
     private String name;
     private Double kcal;
+    private String maker;
     private LocalDateTime takenDateTime;
     private List<String> allergiesWithUser;
 
@@ -27,6 +28,7 @@ public class TakenFoodListServiceDto {
         this.foodId = takenFoodListDto.getFoodId();
         this.name = takenFoodListDto.getName();
         this.kcal = takenFoodListDto.getKcal();
+        this.maker = takenFoodListDto.getMaker();
         this.takenDateTime = takenFoodListDto.getTakenDateTime();
         this.allergiesWithUser = userAllergies.stream()
                 .filter(allergy -> takenFoodListDto.getMaterials().contains(allergy))
